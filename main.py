e90b6bbf20fb1488f7162ab5eec7e9a86a2a5261
Ojirokim <magicojiro@gmail.com>
2026-01-05 09:53:27 +0900

    Initial commit: Sparta Python Basic

---
+++ main.py
@@ -0,0 +1,8 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# what is debug mode that uses red dots
+# Ctrl + R replace all
+
