359ad8c52d0ca75417a3a1bfee3f01e2249c2599
Ojirokim <magicojiro@gmail.com>
2026-01-05 10:47:53 +0900

    Change files

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-# what is debug mode that uses red dots
-# Ctrl + R replace all
-
+print('123')
